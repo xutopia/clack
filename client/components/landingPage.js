8a7b4ddcd5b11b80951a3aa5e36e2a458a7afb13
xutopia <xuology@gmail.com>
2017-02-09 15:48:06 -0500

    create landingPage component that renders a usernameInput container component

---
+++ client/components/landingPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import UsnInput from '../containers/username_input';
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <UsnInput />
+      </div>
+    );
+  }
+}
