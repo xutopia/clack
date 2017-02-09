d282f7cda560f07ef84cfcc230a58aba8f1df8b7
xutopia <xuology@gmail.com>
2017-02-09 15:41:04 -0500

    added app component to be rendered

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {createStore, applyMiddleWare} from 'redux';
 import {Router} from 'react-router';
 import promise from 'redux-promise';
 import reducers from '../reducers';
+import routes from './routes';
 
 // store below is in case we need to create store with middleware
 /* const createStoreWithMiddleware = applyMiddleWare(promise)(createStore); */
