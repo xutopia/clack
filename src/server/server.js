06db1606501059f5aa94a47f6409dc2f2e233271
Peter Schussheim <peter@schussheim.com>
2017-02-10 10:52:47 -0500

    stash before rebase

--- src/server/server.js
+++ src/server/server.js
@@ -1,2 +1,21 @@
-var express = require('express');
-var dotenv = require('dotenv').config();
+const express = require('express')
+const io = require('socket.io')
+const http = require('http')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+app.listen('listening on port', port)
