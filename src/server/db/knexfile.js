a8bf2d37717bd3ed33b3371ecc5c576b1c9bc090
Michelle Carter <mbcarterdev@gmail.com>
2017-02-10 11:07:01 -0500

    adds knex and migration files

---
+++ src/server/db/knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+var host =  process.env.RDS_HOSTNAME || 'localhost';
+var user =  process.env.RDS_USERNAME || 'root';
+var password = process.env.RDS_PASSWORD || '1234';
+var port = process.env.RDS_PORT || '3306'
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host,
+      user,
+      password,
+      port
+    }
+  },
+
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
