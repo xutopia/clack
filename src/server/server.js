fd26a5510f6ba7f5b96a971d92d48b55d59bdb18
Peter Schussheim <peter@schussheim.com>
2017-02-10 19:45:26 -0500

    add socket listeners and events

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,81 @@
-const express = require('express')
-const io = require('socket.io')
-const http = require('http')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
 const app = express();
-
 app.use(bodyParser.urlencoded({
   extended: true,
-}))
-app.use(bodyParser.json())
-app.use(morgan('dev'))
+}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname  }/../../static`));
 
 
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
-app.listen('listening on port', port)
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+
+// Chatroom
+let numUsers = 0;
+
+io.on('connection', (socket) => {
+  let addedUser = false;
+
+  socket.on('new message', (data) => {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data,
+    });
+  });
+
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers,
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers,
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username,
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username,
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers,
+      });
+    }
+  });
+});
