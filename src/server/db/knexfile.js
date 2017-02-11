df279f4f1dbd5ca0eb530d626a01cf1f1249e74c
Michelle Carter <mbcarterdev@gmail.com>
2017-02-10 19:43:28 -0500

    sets up database migration

--- src/server/db/knexfile.js
+++ src/server/db/knexfile.js
@@ -1,22 +1,24 @@
-// Update with your config settings.
-var host =  process.env.RDS_HOSTNAME || 'localhost';
-var user =  process.env.RDS_USERNAME || 'root';
-var password = process.env.RDS_PASSWORD || '1234';
-var port = process.env.RDS_PORT || '3306'
+// Note that the sensitive data for this connection is stored in the .env file.
+
+const host = process.env.RDS_HOSTNAME || 'localhost';
+const user = process.env.RDS_USERNAME || 'root';
+const password = process.env.RDS_PASSWORD || '1234';
+const port = process.env.RDS_PORT || '3306';
 
 module.exports = {
 
   development: {
     client: 'mysql',
     connection: {
+      database: 'clack_rds',
       host,
       user,
       password,
-      port
-    }
+      port,
+    },
   },
 
   migrations: {
-    tableName: 'knex_migrations'
-  }
+    tableName: 'knex_migrations',
+  },
 };
--- src/server/db/migrations/create_db.js
+++ src/server/db/migrations/create_db.js
@@ -1,21 +1,22 @@
-//need to put code for schemas here
+var Promise = require('bluebird');
 
-// exports.up = function(knex, Promise) {
-//   return Promise.all([knex.schema.createTableIfNotExists('users', function (table) {
-//     table.increments('userid');
-//     table.string('username').unique();
-//     table.string('password');
-//   }),
-//   knex.schema.createTableIfNotExists('goals', function(table) {
-//     table.increments('goalid');
-//     table.string('goal');
-//     table.text('description');
-//     table.string('status');
-//     table.timestamps();
-//     table.integer('iduser').references('userid').inTable('users');
-//   })]);
-// };
-//
-// exports.down = function(knex, Promise) {
-//   return Promise.all([knex.schema.dropTable('users'), knex.schema.dropTable('goals')]);
-// };
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    // knex.raw('SET foreign_key_checks = 0;'),
+  knex.schema.createTableIfNotExists('users', function (table) {
+    table.increments('userid');
+    table.string('username').unique();
+    table.string('password');
+  }),
+  knex.schema.createTableIfNotExists('messages', function (table) {
+    table.increments('messageid');
+    table.text('message');
+    table.timestamp('timestamp');
+    table.integer('iduser').unsigned();
+    table.foreign('iduser').references('userid').inTable('users');
+  })]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([knex.schema.dropTable('users'), knex.schema.dropTable('messages')]);
+};
