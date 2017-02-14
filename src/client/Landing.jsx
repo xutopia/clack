1c883bf1b978b072b42367013b435bcc56eee960
xutopia <xuology@gmail.com>
2017-02-14 15:41:30 -0500

    hacked the redirect by making a button. Can successfully redirect to chat room from landing page

--- src/client/Landing.jsx
+++ src/client/Landing.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import { Router } from 'react-router';
+import { Link } from 'react-router';
 
 // input
 
 export default class Landing extends Component {
+  static get contextTypes() {
+    return {
+      router: React.PropTypes.object.isRequired
+    };
+  }
+
   constructor(props, context) {
     super(props, context)
     console.log('context.router', context.router);
     this.state = {
       currentUser: '',
     }
-  }
-
-  static contextTypes = {
-    router: React.PropTypes.object.isRequired
-  };
-
-  componentDidMount() {
-    const { router } = this.context;
   }
 
   onInputChange(event, state) {
@@ -31,7 +29,7 @@ export default class Landing extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     console.log('inside form submit, context', this.router);
-    router.transitionTo('/messages');
+    // router.transitionTo('/messages');
     console.log('the form was submitted');
     // transition to the chat room
   }
@@ -41,6 +39,9 @@ export default class Landing extends Component {
       <div>
         <form onSubmit={this.onFormSubmit}>
           <input type="text" placeholder="Enter Name to Chat" value={currentUser} onChange={event => this.onInputChange(event, this.state)} />
+          <Link to="/messages">
+            <button type="submit">Submit</button>
+          </Link>
         </form>
       </div>
     );
