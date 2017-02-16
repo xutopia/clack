95fea584fee50484c4b5e8d6a5c69b039725a9a1
xutopia <xuology@gmail.com>
2017-02-16 17:28:57 -0500

    test(database): created and passed tests for messages model

--- src/__tests__/database/database.test.js
+++ src/__tests__/database/database.test.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import knex from 'knex';
 import db from '../../server/db/knexfile';
 import createUser from '../../server/user/userModel';
-
+import { createMessage, fetchMessages } from '../../server/messages/messagesModel';
 
 describe('Database queries using knex for users', () => {
   it('Should fetch all the users from database', () => {
@@ -23,18 +23,16 @@ describe('Database queries using knex for users', () => {
 });
 
 describe('Database queries using knex for messages', () => {
-  it('Should fetch all the users from database', () => {
-    const expected = [{ password: null, userid: 1, username: 'michelle' }];
-    db.raw(`SELECT * FROM users`)
-    .then((usernames) => {
-      expect(usernames[0]).toEqual(expect.arrayContaining(expected));
+  it('Should fetch all the messages from database', () => {
+    fetchMessages(null, (messages) => {
+      expect(messages.length).toBeGreaterThanOrEqual(0);
     });
   });
 
   it('Should reject the insert query if username already exists', () => {
-    const newEntry = 'bob';
-    createUser(newEntry, (results) => {
-      expect(results).toBeUndefined();
+    const newMessage = 'Hello World';
+    createMessage(newMessage, (results) => {
+      expect(results).toBeTruthy();
     });
   });
 });
