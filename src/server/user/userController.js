f54b7b9d26fd2a66c9b34fdbc9b3a1005ee0423c
xutopia <xuology@gmail.com>
2017-02-15 19:06:55 -0500

    build(package.json): add commitizen to project

--- src/server/user/userController.js
+++ src/server/user/userController.js
@@ -1,1 +1,2 @@
 // The controller for the user actions
+import userModel from './userModel';
--- src/server/user/userModel.js
+++ src/server/user/userModel.js
@@ -1,1 +1,15 @@
 // The actual db queries for the user
+import db from '../db/knexfile';
+
+const createUser = (data, callback) => {
+  // const { name } = data;
+
+  return db.run(`SELECT * FROM users`)
+  .then((usernames) => {
+    console.log(usernames);
+  });
+};
+
+createUser();
+
+export default createUser;
