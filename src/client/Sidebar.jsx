651927f5bd18c5d179b3ccfa7b75e746e36a2da7
Michelle Carter <mbcarterdev@gmail.com>
2017-02-17 12:23:39 -0500

    fix($components): work on Sidebar component
    Sidebar still needs a fetchUsers function in the actions folder, also needs SidebarItem defined

--- src/client/Sidebar.jsx
+++ src/client/Sidebar.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import SidebarItem from './SidebarItem';
+// import the actions file for the fetchusers function that still needs to be written.
+// NOTE THAT THIS FILE NEEDS REFACTORING FOR REDUX FUNCTIONALITY
+// NEED TO DEFINE SidebarItem
 
 export default class Sidebar extends Component {
   componentWillMount() {
