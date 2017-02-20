3d7d7663e9a07b4fc974788b84d4bf2332742e31
Peter Schussheim <peter@schussheim.com>
2017-02-20 14:54:43 -0500

    refactor(UI routes): refactor app into stateless component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Landing from './Landing'
 import Room from './Room'
 
 import { logout } from './actions/actions'
 
-class App extends React.Component {
-  handleLogout() {
-    this.props.dispatch(logout());
-  }
-
-  render() {
-    return (
+const App = () => {
+  return (
+    <Router>
       <div>
-        {this.props.children}
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/room" component={Room} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 function select({ app }) {
--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 
 import configureStore from './client/store/index';
 
 import App from './client/App';
-import Landing from './client/Landing';
-import Room from './client/Room';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <Router>
-      <App>
-        <div>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/room" component={Room} />
-        </div>
-      </App>
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('container')
 );
