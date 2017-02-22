50c9c84ce3f0f0552ef2bb96305c7a3de99e6de7
Peter Schussheim <peter@schussheim.com>
2017-02-22 10:51:26 -0500

    update app entry point config to properly support HMR

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
+// import { AppContainer } from 'react-hot-loader'
+import { Provider } from 'react-redux'
 
 import configureStore from './client/store/index';
-
 import App from './client/App';
 
-ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
-  document.getElementById('container')
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <Provider store={configureStore()}>
+      <Component />
+    </Provider>,
+    document.getElementById('root')
+  );
+}
+
+render(App)
+
+module.hot.accept('./App', () => {
+  render(App)
+})
