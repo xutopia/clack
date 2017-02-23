fce8ae505ee43b1d02580dbb7207df3b2cc3cbbb
xutopia <xuology@gmail.com>
2017-02-22 19:56:18 -0500

    feat(search): implemented search index to have list of messages

--- src/client/util/indexMessages.js
+++ src/client/util/indexMessages.js
@@ -3,9 +3,20 @@ import lunr from 'lunr';
 
 const generateMessagesIndex = (messages) => {
   const idx = lunr(() => {
-    this.field('title', { boost: 10 })
-    this.field('body')
+    this.field('username', { boost: 10 })
+    this.field('text')
   });
+
+  for(let key in messages) {
+    const doc = {
+      text: messages.text,
+      username: messages.username,
+      id: key,
+    }
+    idx.add(doc);
+  }
+
+  return idx;
 }
 
 export default generateMessagesIndex;
