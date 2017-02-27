f879a19295bdc6f2573ffe389f8731f87cef690d
Peter Schussheim <peter@schussheim.com>
2017-02-27 14:54:14 -0500

    feat(actions.js): add actions for new login flow

--- src/client/actions/actions.js
+++ src/client/actions/actions.js
@@ -1,5 +1,6 @@
 import { createAction } from 'redux-act';
 
+// old 'login' using sockets...will be deprecated
 export const login = createAction('login');
 export const logout = createAction('logout');
 
@@ -16,3 +17,12 @@ export const searchMessages = createAction('new search');
 
 export const addReaction = createAction('add reaction');
 export const sendUpdatedReaction = createAction('updated reaction');
+
+// new auth using simple bcrypt
+export const sendingRequest = createAction('sending auth request');
+export const loginRequest = createAction('login request');
+export const registerRequest = createAction('registration request');
+export const setAuth = createAction('set auth');
+export const logoutRequest = createAction('logout request');
+export const changeForm = createAction('change form');
+export const requestError = createAction('request error');
