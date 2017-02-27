a7d1884603c88b4011c9f4aad747f6b1f497ebeb
Peter Schussheim <peter@schussheim.com>
2017-02-27 14:55:00 -0500

    feat(sagas/index.js): add new auth sagas to rootSaga

--- src/client/sagas/index.js
+++ src/client/sagas/index.js
@@ -2,8 +2,10 @@ import io from 'socket.io-client';
 import { eventChannel } from 'redux-saga';
 import { fork, take, call, put, cancel } from 'redux-saga/effects';
 import {
-  login, logout, addUser, removeUser, newMessage, sendMessage,
+  login, logout, addUser, removeUser, newMessage, sendMessage
 } from '../actions/actions';
+
+import { loginFlow, logoutFlow, registerFlow } from './auth'
 
 function connect() {
   const socket = io('http://localhost:3000');
@@ -59,7 +61,7 @@ function* handleIO(socket) {
   by our saga which then cancels the task.
 */
 
-function* loginFlow() {
+function* loginFlowSockets() {
   while (true) {
     let { payload } = yield take(`${login}`);
     const socket = yield call(connect);
@@ -74,5 +76,8 @@ function* loginFlow() {
 }
 
 export default function* rootSaga() {
+  yield fork(loginFlowSockets);
   yield fork(loginFlow);
+  yield fork(logoutFlow);
+  yield fork(registerFlow);
 }
