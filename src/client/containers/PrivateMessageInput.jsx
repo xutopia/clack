384c4f6fe1e4e50e735e80f4812483933c79ffd2
xutopia <xuology@gmail.com>
2017-02-28 18:49:40 -0500

    feat(privateMsg): complete private message user input fields and action dispatch to send private mes

--- src/client/containers/PrivateMessageInput.jsx
+++ src/client/containers/PrivateMessageInput.jsx
@@ -13,21 +13,39 @@ class PrivateMessageInput extends Component {
     }
   }
 
-  handlePrivateSend = (event) => {
-
+  onPrivateMessageSubmit = (event) => {
+    const target = this.state.secretRecipient;
+    const text = this.state.secretMessage;
+    this.props.dispatch(sendPrivateMessage({ target, text }));
+    this.setState({
+      secretRecipient: '',
+      secretMessage: '',
+    });
+    event.preventDefault();
   }
+
+  handleRecipientInput = (event, state) => {
+    const value = event.target.value;
+    this.setState({ ...state, secretRecipient: value });
+  }
+
+  handleMessageInput = (event, state) => {
+    const value = event.target.value;
+    this.setState({ ...state, secretMessage: value });
+  }
+
 
   render() {
     return (
       <div>
-        <form onSubmit={(e) => this.onSearchSubmit(e)}>
+        <form onSubmit={(e) => this.onPrivateMessageSubmit(e)}>
           Secret Message Recipient: <br />
           <Input
             type="text"
             id="input-secretMsg-recipient"
             placeholder="receiver's name"
             value={this.state.secretRecipient}
-            onChange={(event) => this.handleRecipientInput(event)}
+            onChange={(event) => this.handleRecipientInput(event, this.state)}
           />
           Secret Message: <br />
           <Input
@@ -35,7 +53,7 @@ class PrivateMessageInput extends Component {
             id="input-secretMsg-content"
             placeholder="message content"
             value={this.state.secretMessage}
-            onChange={(event) => this.handleMessageInput(event)}
+            onChange={(event) => this.handleMessageInput(event, this.state)}
           />
         </form>
       </div>
