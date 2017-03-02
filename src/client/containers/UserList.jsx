50d0a29ef8b42de651d3c29e9c48875a694a0b21
xutopia <xuology@gmail.com>
2017-03-02 09:51:07 -0500

    feat(UX): add a 'me' indicator to let user know which name is his hers

--- src/client/containers/UserList.jsx
+++ src/client/containers/UserList.jsx
@@ -14,17 +14,22 @@ class UserList extends React.Component {
     return (
       <div>
         <div>
-          {usernames ? (usernames.map((user, index) => (
-            <UserItem user={user} key={index} />
-          ))) : (<div>Loading...</div>)}
+          {usernames ? (usernames.map((user, index) => {
+            if(this.props.app.username === user) {
+              const me = `${user} (me)`;
+              return (<UserItem user={me} key={index} />)
+            } else {
+              return (<UserItem user={user} key={index} />)
+            }
+          })) : (<div>Loading...</div>)}
         </div>
       </div>
     )
   }
 }
 
-function mapStateToProps({ usernames, users }) {
-  return { usernames, users };
+function mapStateToProps({ app, usernames, users }) {
+  return { app, usernames, users };
 }
 
 export default connect(mapStateToProps)(UserList)
