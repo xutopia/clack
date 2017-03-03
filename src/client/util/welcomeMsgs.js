e82916414e1782028748dba2eb01f6a757eaaa22
xutopia <xuology@gmail.com>
2017-03-03 12:45:17 -0500

    feat(welcome): Create list of welcome messages to display in util

---
+++ src/client/util/welcomeMsgs.js
@@ -0,0 +1,11 @@
+// an array of welcome messages for the splash page
+
+const welcomeMsgs = [
+  "What is your name?",
+  "Please enter your name!",
+  "We want your name!",
+  "Name?",
+  "Name please!",
+]
+
+export default welcomeMsgs;
