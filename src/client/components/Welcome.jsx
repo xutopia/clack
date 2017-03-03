995bef959296adf0a07d3c3970ed5f2c0dc95298
xutopia <xuology@gmail.com>
2017-03-03 12:36:19 -0500

    feat(welcome): create welcome component template to go in Landing

---
+++ src/client/components/Welcome.jsx
@@ -0,0 +1,13 @@
+// landing splash page with typing animation
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Welcome = () => {
+  return (
+    <div>
+      Probably my last component for now
+    </div>
+  )
+}
+
+export default connect()(Welcome);
