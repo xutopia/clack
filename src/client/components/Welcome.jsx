557b467e7d54b01e09ae93b92fbe32e07f7f45d5
xutopia <xuology@gmail.com>
2017-03-03 12:45:17 -0500

    feat(welcome): Create list of welcome messages to display in util

--- src/client/components/Welcome.jsx
+++ src/client/components/Welcome.jsx
@@ -1,10 +1,16 @@
 // landing splash page with typing animation
 import React from 'react';
 import { connect } from 'react-redux';
+import welcomeMsgs from '../util/welcomeMsgs';
 
 const Welcome = () => {
   return (
     <div>
+      <h1>Wecome to Clack!</h1>
+      <span
+        class="txt-rotate"
+        data-period="2000"
+        data-rotate={welcomeMsgs}
       Probably my last component for now
     </div>
   )
